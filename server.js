29bd1a2e2028c0a73d7cc784e56fc17b7a0ef252
unknown <damianjnc@gmail.com>
2019-07-05 02:42:44 +0100

    express static site template

--- server.js
+++ server.js
@@ -2,26 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.static(__dirname + '/public'))
 
-app.use(bodyParser.urlencoded({extended: false}))
-
-
-app.get('/', (req, res) => {
-    res.send('getting root');
-});
-
-app.get('/profile', (req, res) => {
-
-    res.send('getting profile');
-});
-
-app.post('/profile', (req, res) => {
-    console.log(req.body);
-    const user = {
-        name: 'sally',
-        hobby: 'bjj'
-    }
-    res.send(user);
-});
 
 app.listen(3000);
